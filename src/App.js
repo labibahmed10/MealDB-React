93c53a308df4b3f84418b3474384816a6832ed01
labibahmed10 <labib.ahmed.372@gmail.com>
2022-03-18 18:18:06 +0600

    updating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Blog from "./component/Blog";
+import Mobile from "./component/Mobile";
+import Posts from "./component/Posts";
+
+const blog = {
+  color: "purple",
+  backgroundColor: "lavender",
+  padding: "20px",
+  marginTop: "30px",
+  textAlign: "justify",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article style={blog}>
+        <h2 style={{ textAlign: "center" }}>Every blogs of some of the writers</h2>
+        <Blog></Blog>
+        <Blog></Blog>
+        <Mobile></Mobile>
+        <Posts></Posts>
+      </article>
     </div>
   );
 }
