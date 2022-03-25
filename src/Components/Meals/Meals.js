7f041bdab2f934680252b27e7d02f0796d45ad10
labibahmed10 <labib.ahmed.372@gmail.com>
2022-03-25 12:12:28 +0600

    updated

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Meals.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+
 const Meals = ({ meal }) => {
   console.log(meal);
   const { strMealThumb, strArea, strCategory, strMeal, strInstructions } = meal;
@@ -8,11 +11,12 @@ const Meals = ({ meal }) => {
     <div className="food-info">
       <img src={strMealThumb} alt="" />
       <h1>{strArea}</h1>
-      <h2>{strCategory}</h2>
-      <p>{strMeal}</p>
-      <p>{strInstructions.length > 30 ? strInstructions.slice(0, 150) : strInstructions}</p>
+      <h2>{strMeal}</h2>
+      <p>{strInstructions.length > 30 ? strInstructions.slice(0, 130) : strInstructions}</p>
 
-      <button>Add to Cart</button>
+      <button>
+        Add to Cart <FontAwesomeIcon icon={faCartPlus}></FontAwesomeIcon>
+      </button>
     </div>
   );
 };
